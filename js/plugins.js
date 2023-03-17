248df4402ec788c672ad480ad09303ad10caad7d
PattaLab <127221478+pattalab@users.noreply.github.com>
2023-03-16 17:20:51 -0700

    un

--- js/plugins.js
+++ js/plugins.js
@@ -122,19 +122,7 @@ function initLightbox( options ){
 		navigationOff = function()
 		{
 			$( '#imagelightbox-nav' ).remove();
-
-
-		// Get the offset position of the navbar
-		var sticky = navbar.offsetTop;
-
-		// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-		function myFunction() {
-		if (window.pageYOffset >= sticky) {
-			navbar.classList.add("sticky")
-		} else {
-			navbar.classList.remove("sticky");
-		}
-		}
+		},
 
 
 		// ARROWS
@@ -279,4 +267,4 @@ function initLightbox( options ){
 	})
 
 }(jQuery);
-}
+
